000113304f3d1a89a88aea175d75e6733d423934
rnjstjdqhd <rnjstjdqhd3939@gmail.com>
2022-10-05 20:05:18 +0900

    init

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,22 +3,53 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+          <div>
+              test
+              test
+              test
+          </div>
+          <div className="d-flex">
+              <div className="p-2 flex-fill item">Flex item with a lot of content</div>
+              <div className="p-2 flex-fill item">Flex item</div>
+              <div className="p-2 flex-fill item">Flex item</div>
+          </div>
+          <div className="d-flex">
+              <div className="p-2 flex-fill item">Flex item with a lot of content</div>
+              <div className="p-2 flex-fill item">
+                  <div>test</div>
+                  <div>test</div>
+                  <div>test</div>
+              </div>
+              <div className="p-2 flex-fill item">Flex item</div>
+          </div>
+          <div className="d-flex">
+              <div className="p-2 flex-grow-1 item">Flex item with a lot of content</div>
+              <div className="p-2 item">Flex item</div>
+          </div>
+          <div style={{border: "solid 1px black"}}>
+              <div className="d-flex">
+                  <div className="flex-shrink-0">
+                      <img src={logo} className="App-logo" alt="logo" style={{height:"50px", width:"50px"}} />
+                  </div>
+                  <div className="flex-grow-1 ms-3">
+                      <div>This is some content from a media component.</div>
+                      <div>You can replace this with any content and adjust it as needed.</div>
+                  </div>
+              </div>
+          </div>
+          <div style={{border: "solid 1px black"}}>
+              <div className="d-flex align-items-center">
+                  <div className="flex-shrink-0">
+                      <img src={logo} className="App-logo" alt="logo" style={{height:"50px", width:"50px"}} />
+                  </div>
+                  <div className="flex-grow-1 ms-3">
+                      <div>This is some content from a media component.</div>
+                      <div>You can replace this with any content and adjust it as needed.</div>
+                  </div>
+              </div>
+          </div>
+      </div>
   );
 }
 
